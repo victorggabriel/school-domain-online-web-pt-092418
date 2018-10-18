e3f0db97cb95b3c14106854f24c36ab2aaa56fa2
Victor Gonzalez <victorggrodriguez@gmail.com>
2018-10-18 20:40:15 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,4 +1,27 @@
-# code here!
 class School
-  attr_accessor :roster
+ attr_accessor :roster, :school_name
+
+ def initialize(school_name)
+   @roster = Hash.new
+   @roster = roster
+ end
+
+ def add_student(student, grade)
+  @roster[grade] ||= []
+  @roster[grade] << student
+ end
+
+ def grade(student)
+   @roster[student]
+ end
+
+ def sort()
+   new_roster = Hash.new
+   @roster.each do |grade, student|
+     new_roster[grade] = student.sort
+   end
+   new_roster
+ end
+
+
 end
